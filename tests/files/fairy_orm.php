663e6fae0b1f986e2cc2a6161cc841c3c61dd486
dracony <draconyster@gmail.com>
2013-04-18 17:05:06 +0300

    setting composer package

---
+++ tests/files/fairy_orm.php
@@ -0,0 +1,20 @@
+<?php
+namespace Model {
+	class Fairy extends \PHPixie\ORM\Model
+	{
+
+		public $belongs_to = array('tree');
+		public $connection = 'orm';
+		public $has_many = array(
+			'protects' => array('model' => 'tree', 'key' => 'protector_id'),
+			'friends' => array('model' => 'fairy', 'through' => 'friends', 'key' => 'fairy_id', 'foreign_key' => 'friend_id')
+		);
+
+		public function get($column)
+		{
+			if ($column == 'test')
+				return 5;
+		}
+
+	}
+}

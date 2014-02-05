e6bae27d22911802e3d09812862552df2bae5998
dracony <draconyster@gmail.com>
2014-02-05 18:42:34 +0200

    manytomany

--- classes/PHPixie/ORM/Query/Plan/Planner/Cross/Strategy/Ids.php
+++ classes/PHPixie/ORM/Query/Plan/Planner/Cross/Strategy/Ids.php
@@ -4,14 +4,11 @@ namespace \PHPixie\ORM\Query\Plan\Planner\Cross\Strategy;
 
 class Ids extends \PHPixie\ORM\Query\Plan\Planner\Cross\Strategy {
 	
-	public function link() {
-		$keys = array($left_key, $right_key);
-		
+	public function link_plan($left_collection, $right_collection, $left_repository, $right_repository, $left_pivot_key, $right_pivot_key, $plan) {
 		$query = $this->db->query('insert', $config['pivot_connection']);
 		$this->set_collection($query, $config['pivot']);
 		
-		$insert_step = $this->steps->cross_insert($query, $keys);
-		
+		$insert_step = $this->steps->cross_insert($query, array($left_key, $right_key));
 		$insert_step->add_left_ids($left_collection->field($left_id_field) , true);
 		$insert_step->add_right_ids($right_collection->field($right_id_field) , true);
 		
@@ -29,5 +26,41 @@ class Ids extends \PHPixie\ORM\Query\Plan\Planner\Cross\Strategy {
 			}
 		
 		$plan->push($insert_step);
+		return $plan;
+	}
+	
+	public function unlink_plan($left_collection, $right_collection, $left_repository, $right_repository, $left_pivot_key, $right_pivot_key, $plan) {
+		$query = $this->db->query('insert', $config['pivot_connection']);
+		$this->set_collection($query, $config['pivot']);
+		
+		$insert_step = $this->steps->cross_delete($query, array($left_key, $right_key));
+		$insert_step->add_left_ids($left_collection->field($left_id_field) , true);
+		$insert_step->add_right_ids($right_collection->field($right_id_field) , true);
+		
+		$collections = array(
+			'left' => $left_collection,
+			'right' => $right_collection,
+		);
+		
+		foreach($collections as $side => $collection)
+			foreach($collection->queries() as $query) {
+				$subplan = $query->map();
+				$subquery = $subplan->pop_result_query();
+				$plan->prepend_plan($subplan);
+				$insert_step->add_query($side, $subquery);
+			}
+		
+		$plan->push($insert_step);
+		return $plan;
+	}
+	
+	protected function add_collection($collection, $side, $id_field, $insetstep) {
+		$step->add_ids($side, $collection->field($id_field), true);
+		foreach($collection->queries() as $query) {
+			$subplan = $query->map();
+			$subquery = $subplan->pop_result_query();
+			$plan->prepend_plan($subplan);
+			$insert_step->add_query($side, $subquery);
+		}
 	}
 }
--- classes/PHPixie/ORM/Query/Plan/Planner/Cross/Strategy/Subquery.php
+++ classes/PHPixie/ORM/Query/Plan/Planner/Cross/Strategy/Subquery.php
@@ -4,61 +4,56 @@ namespace \PHPixie\ORM\Query\Plan\Planner\Cross\Strategy;
 
 class Subquery extends \PHPixie\ORM\Query\Plan\Planner\Cross\Strategy {
 	
-	public function link() {
-		$keys = array($left_key, $right_key);
+	public function link_plan($left_collection, $right_collection, $left_repository, $right_repository, $left_pivot_key, $right_pivot_key, $plan) {
+		$left_query = $this->collection_query($left_collection, $left_repository, $left_pivot_key, $plan);
+		$right_query = $this->collection_query($right_collection, $right_repository, $right_pivot_key, $plan);
 		
-		$query->
-		$query = $this->db->query('insert', $config['pivot_connection']);
-		$this->set_collection($query, $config['pivot']);
-		
-		
-		$insert_step = $this->steps->cross_insert($query, $keys);
-		
-		$queries = array();
-		
-		$sides = array(
-			'left' => array(
-				'collection' => $left_collection,
-				'repository' => $left_repository,
-				'id_field' => $left_repository->id_field(),
-				'pivot_key'  => $left_key
-			),
-			'right' => array(
-				'collection' => $right_collection,
-				'repository' => $right_repository,
-				'id_field' => $right_repository->id_field(),
-				'pivot_key'  => $right_key
-			),
-		);
-		
-		foreach($sides as $side_name => $side) {
-		
-			$queries[$side_name] = $side['repository']->collection()->query('select')
-																	->fields(array($side['id_field']));
-			$inner_query = null;
-			foreach($side['collection']->get_ids($side['id_field'], true) as $id)
-				$inner_query = $this->union(
-										$inner_query, 
-										$this->id_query($side['repository'], $side['id_field'], $id)
-								);
-			
-			foreach($side['collection']->queries() as $orm_query) 
-				$inner_query = $this->union(
-										$inner_query,
-										$this->map_orm_query($orm_query, $plan, $side['id_field'])
-								);
-			
-			if ($inner_query === null)
-				throw new \PHPixie\Exception\Mapper("No ids or queries set for pivot '{$side['pivot_key']}' field.");
-			
-			$queries[$side_name]->table($inner_query, "cross_insert_{$side_name}_side");
-		}
-		
-		$insert_query = $this->db->query('insert', $pivot_connection)
+		$query = $this->db->query('insert', $pivot_connection)
 												->table($pivot_table)
-												->batch_insert($keys, $queries['left']->join($queries['right'], 'cross'));
+												->batch_insert(
+													array($left_key, $right_key),
+													$left_query->join($right_query, 'cross')
+												);
 												
-		$plan->push($insert_query);
+		$plan->push($this->steps->query($query));
+		return $plan;
+	}
+	
+	public function unlink_plan($left_collection, $right_collection, $left_repository, $right_repository, $left_pivot_key, $right_pivot_key, $plan) {
+		$left_query = $this->collection_query($left_collection, $left_repository, $left_pivot_key, $plan);
+		$right_query = $this->collection_query($right_collection, $right_repository, $right_pivot_key, $plan);
+		
+		$query = $this->db->query('delete', $pivot_connection)
+												->table($pivot_table)
+												->where($left_key, 'in', $left_query)
+												->where($right_key, 'in', $right_query)
+												
+		$plan->push($this->steps->query($query));
+		return $plan;
+	}
+	
+	protected function collection_query($collection, $repository, $pivot_key, $plan) {
+		$id_field = $repository->id_field();
+		$query = $collection()->query('select')
+												->fields(array($id_field));
+		$inner_query = null;
+		foreach($collection->get_ids($id_field, true) as $id)
+			$inner_query = $this->union(
+									$inner_query, 
+									$this->id_query($repository, $id_field, $id)
+								);
+			
+		foreach($collection->queries() as $orm_query) 
+			$inner_query = $this->union(
+									$inner_query,
+									$this->map_orm_query($orm_query, $plan, $id_field)
+								);
+			
+		if ($inner_query === null)
+			throw new \PHPixie\Exception\Mapper("No ids or queries set for pivot '{$pivot_key}' field.");
+			
+		$query->table($inner_query, "cross_{$pivot_key}");
+		return $query;
 	}
 	
 	protected function union($query, $subquery) {

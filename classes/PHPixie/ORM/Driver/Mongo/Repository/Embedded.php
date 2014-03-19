14f62202edb1db22ccc5cef1cf7e90ea07869105
Roman Tsiupa <draconyster@gmail.com>
2014-03-19 20:55:34 +0200

    merge

--- classes/PHPixie/ORM/Driver/Mongo/Repository/Embedded.php
+++ classes/PHPixie/ORM/Driver/Mongo/Repository/Embedded.php
@@ -2,28 +2,17 @@
 
 namespace PHPixie\ORM\Driver\Mongo\Repository;
 
-class Embedded extends \PHPixie\ORM\Driver\Mongo\Repository
+class Embedded
 {
-    protected $path;
-
-    public function __construct($modelName, $connection, $pluralName, $config)
+    protected $parent;
+    protected $class;
+    
+    public function __construct($parent, $config)
     {
-        parent::__construct($modelName, $connection, $pluralName, $config);
-        $this->collection = $config->get('collection');
-    }
-
-    public function dbQuery($type)
-    {
-        return $this->orm->embeddedQuery($type, $this->connection);
-    }
-
-    public function idField()
-    {
-        return $this->path.'.'.$this->idField;
+        $this->parent = $parent;
+        $this->class = $config->get('class');
     }
     
-    public function delete()
-    {
-        
-    }
+    
+
 }

c62f108ed49e511df18e6aa1cf6b64f09d21647a
Roman Tsiupa <draconyster@gmail.com>
2014-03-22 14:49:05 +0200

    data storage

--- classes/PHPixie/ORM/Model/Data.php
+++ classes/PHPixie/ORM/Model/Data.php
@@ -3,61 +3,82 @@
 namespace PHPixie\ORM\Model;
 
 class Data {
-    
-    protected $data;
-    protected $modified = array();
-    protected $nested = array();
-    protected $cached = array();
-    
-    public function __construct($data)
-    {
-        $this->data = (array) $data;
-    }
-    
-    public function get($key)
-    {
-        $this->prepareProperty($key);
-        
-        if (array_key_exists($key, $this->modified))
-            return $this->modified[$key];
-        
-        if (array_key_exists($key, $this->nested))
-            return $this->nested[$key];
-        
-        if (array_key_exists($key, $this->data))
-            return $this->data[$key];
-        
-        $args = func_get_args();
-        if (array_key_exists(1, $args))
-            return $args[1];
-        
-        throw new \PHPixie\ORM\Exception\Model("Property '$key' not found");
-        
-    }
-    
-    public function set($key, $value)
-    {
-        $value = $this->setDataValue($key, $value);
-        $this->modified[$key] = $value;
-    }
-    
-    public function __set($key, $value)
-    {
-        $this->set($key, $value);
-    }
-    
-    public function asArray()
-    {
-        $arr = $this->modified;
-        
-        foreach($this->nested as $key => $nested)
-            if(!array_key_exists($key, $arr))
-                $arr[$key] = $nested->asArray();
-        
-        foreach($this->data as $key => $data)
-            if(!array_key_exists($key, $arr))
-                $arr[$key] = $data;
-                
-        return $arr;                
-    }
+	protected $data;
+	protected $target;
+	protected $propertiesSet = false;
+	
+	public function __construct($data, $target = null)
+	{
+		$this->data = get_object_vars($data);
+		$this->target = $target !== null ? $target : $this;
+	}
+	
+	public function setDataProperties()
+	{
+		foreach($this->data as $key => $value) {
+			$this->target->$key = $this->normalizeValue($value);
+		}
+	}
+	
+	public function addSubdocument($key, $data = null)
+	{
+		if ($data === null) {
+			$data = new \stdClass;
+		}elseif($data instanceof static){
+			$data = $data->getCurrentData()
+		}elseif($data instanceof \stdClass) {
+			$data = $this->buildData($data);
+		}else
+			throw new \PHPixie\ORM\Exception\Model("Subdocument data must be a {get_class($this)} or stdClass instance.");
+		}
+		$this->target->$key = $this->buildData($data);
+	}
+	
+	public function buildData($data)
+	{
+		return new static($data);
+	}
+	
+	public function getCurrentData() {
+		$targetData = get_object_vars($this->target);
+		$classProperties = get_class_vars(get_class($this->target));
+		foreach($classProperties as $property)
+			unset($targetData[$property]);
+			
+		$currentData = new \stdClass;
+		foreach($targetData as $key => $value)
+			$currentData->$key = $value;
+		return $currentData;
+	}
+	
+	public function getModified()
+	{
+		$old = $this->data;
+		$current = $this->getCurrentData();
+		
+		$oldProperties = get_object_vars($old);
+		$currentProperties = get_object_vars($current);
+		
+		$unset = a
+		$set = array();
+		$unset = array();
+		$pull = array();
+		$push = array();
+		
+	}
+	
+	public function normalizeValue($value)
+	{
+		if ($value !== null && $value instanceof \stdClass)
+		{
+			$value = $this->buildData($value);
+		}
+		
+		return $value;
+	}
+	
 }
+
+$d = new Data();
+$d->d=5;
+$d->d=6;

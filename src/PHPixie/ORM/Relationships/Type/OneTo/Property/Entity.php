a3b67422b70d8de2b13342c372374948ef699e23
Roman Tsiupa <draconyster@gmail.com>
2017-01-12 10:39:21 +0100

    Fixed interface tag

--- src/PHPixie/ORM/Relationships/Type/OneTo/Property/Entity.php
+++ src/PHPixie/ORM/Relationships/Type/OneTo/Property/Entity.php
@@ -3,7 +3,8 @@
 namespace PHPixie\ORM\Relationships\Type\OneTo\Property;
 
 abstract class Entity extends    \PHPixie\ORM\Relationships\Relationship\Implementation\Property\Entity
-                      implements \PHPixie\ORM\Relationships\Relationship\Property\Entity\Query
+                      implements \PHPixie\ORM\Relationships\Relationship\Property\Entity\Query,
+                                 \PHPixie\ORM\Relationships\Relationship\Property\Entity\Data
 {
     protected $handler;
 
